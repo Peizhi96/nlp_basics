441cdbc10c7e0053056146914a79d066c9a2dea0
Peizhi Yan <yanzhenyi123@gmail.com>
2025-04-07 10:57:21 -0400

    word2vec

--- feature_extraction_word2vec.py
+++ feature_extraction_word2vec.py
@@ -1,6 +1,6 @@
 from gensim.models import Word2Vec
 
-def train_word2vec_model(sentences, vector_size=100, window=5, min_count=1, workers=4):
+def train_word2vec_model(sentences, vector_size=100, window=5, min_count=1, workers=2):
     sentences = [sentence.split() for sentence in sentences]
     model = Word2Vec(sentences, vector_size=vector_size, window=window, min_count=min_count, workers=workers)
     return model
